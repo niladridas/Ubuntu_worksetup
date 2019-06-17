9c2aa223f5cb597b64fc299e990c7857d74904bf
NILADRI DAS <niladridas95@gmail.com>
2019-06-17 12:12:42 -0500

    added TO-DO

--- startupubuntu.sh
+++ startupubuntu.sh
@@ -2,3 +2,4 @@
 dropbox start
 # TO-DO
 # Check if ~/.bash_profile or ~/.bash_login file is already present or not
+# Download dropbox and add this startup script to the ~/.profile file automatically
